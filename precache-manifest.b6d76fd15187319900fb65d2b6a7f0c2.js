b957f958e67f92f8fb519685d8ad18d99eab8717
AShok <aknallapareddy@gmail.com>
2020-09-27 01:04:29 +0530

    Updates

---
+++ precache-manifest.b6d76fd15187319900fb65d2b6a7f0c2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47e034ecea095f562d7c99789815627",
+    "url": "/reduxgithubsearch/index.html"
+  },
+  {
+    "revision": "c3ce426b049b5ecc72e1",
+    "url": "/reduxgithubsearch/static/css/2.24e7bf57.chunk.css"
+  },
+  {
+    "revision": "f1ee1f77fe69893a7c89",
+    "url": "/reduxgithubsearch/static/css/main.fd1bf654.chunk.css"
+  },
+  {
+    "revision": "c3ce426b049b5ecc72e1",
+    "url": "/reduxgithubsearch/static/js/2.5884b8fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reduxgithubsearch/static/js/2.5884b8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ee1f77fe69893a7c89",
+    "url": "/reduxgithubsearch/static/js/main.7f8ca5e3.chunk.js"
+  },
+  {
+    "revision": "838e12130e8a5b0b9200",
+    "url": "/reduxgithubsearch/static/js/runtime-main.9b1552ab.js"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/reduxgithubsearch/static/media/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/reduxgithubsearch/static/media/arrow_right.d9ccc77c.svg"
+  },
+  {
+    "revision": "1540053d6c0df3d33330b7cfec2b60eb",
+    "url": "/reduxgithubsearch/static/media/bg-grocery.1540053d.jpg"
+  },
+  {
+    "revision": "ae37590ab05e9c625bcabf1759ab9dc2",
+    "url": "/reduxgithubsearch/static/media/fa-brands-400.ae37590a.eot"
+  },
+  {
+    "revision": "b5f5ef6112d693b968ad9d1646eea529",
+    "url": "/reduxgithubsearch/static/media/fa-brands-400.b5f5ef61.woff"
+  },
+  {
+    "revision": "c09b5960237e462582aa7b66411e57d5",
+    "url": "/reduxgithubsearch/static/media/fa-brands-400.c09b5960.ttf"
+  },
+  {
+    "revision": "c971847be1cd6c17abca74025b4f36ae",
+    "url": "/reduxgithubsearch/static/media/fa-brands-400.c971847b.woff2"
+  },
+  {
+    "revision": "fb0cbe85f42e49383ac4764fd3211e66",
+    "url": "/reduxgithubsearch/static/media/fa-brands-400.fb0cbe85.svg"
+  },
+  {
+    "revision": "032ba2e0f134a35ed87df1564936d352",
+    "url": "/reduxgithubsearch/static/media/fa-regular-400.032ba2e0.woff2"
+  },
+  {
+    "revision": "562010a46ef5216ac76a08c2ceb99985",
+    "url": "/reduxgithubsearch/static/media/fa-regular-400.562010a4.woff"
+  },
+  {
+    "revision": "6bad016cd4efb36aa82618f55f0f660d",
+    "url": "/reduxgithubsearch/static/media/fa-regular-400.6bad016c.ttf"
+  },
+  {
+    "revision": "c15c758e798dd295b471b69ac409ef02",
+    "url": "/reduxgithubsearch/static/media/fa-regular-400.c15c758e.svg"
+  },
+  {
+    "revision": "d7de79cae74b02f2d377786656f1d816",
+    "url": "/reduxgithubsearch/static/media/fa-regular-400.d7de79ca.eot"
+  },
+  {
+    "revision": "10c304f14cd2f6b6bed2ae7f574f03af",
+    "url": "/reduxgithubsearch/static/media/fa-solid-900.10c304f1.eot"
+  },
+  {
+    "revision": "3f07fd745b951055c656cf27db9a31d5",
+    "url": "/reduxgithubsearch/static/media/fa-solid-900.3f07fd74.ttf"
+  },
+  {
+    "revision": "9948e588ad4ca0db716da1c4d57f900e",
+    "url": "/reduxgithubsearch/static/media/fa-solid-900.9948e588.svg"
+  },
+  {
+    "revision": "c9a328cc89d13b8959e710d82b4b40d1",
+    "url": "/reduxgithubsearch/static/media/fa-solid-900.c9a328cc.woff"
+  },
+  {
+    "revision": "f9b85c9463af7103b9b24bbbf09a06ed",
+    "url": "/reduxgithubsearch/static/media/fa-solid-900.f9b85c94.woff2"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/reduxgithubsearch/static/media/spinner.26ef8bb4.gif"
+  }
+]);
